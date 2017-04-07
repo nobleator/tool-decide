4b19f79e66cd4cccb20a546e5e944f0b62b267e8
jaken96 <jaken96@gmail.com>
2017-04-07 17:27:13 -0400

    Complete overhaul using tabs
    I was wrong when I said it was "built-in" functionality, but I went
    ahead and implemented a tab layout so that slides can be easily
    navigated. I would like to eventually integrate the graph into the
    tab-selector, so that the user can click on the part of the graph that
    they want to change. but before doing anything like that, we need to
    have another talk about layout, and I'd like to see the data structures
    you're working with on the back-end so I can first and foremost ensure
    that I'm collecting the appropriate data from the user.

--- public/js/index.js
+++ public/js/index.js
@@ -1,47 +1,44 @@
-var slides = ['slide1','slide2','slide3'];
-var userNums = {};
-var currentSlide = 'slide1';
-//nextSlide function
-function nextSlide() {
-  if ((slides.indexOf(currentSlide)+1) < slides.length) {
-    document.getElementById(currentSlide).style.display="none";
-    currentSlide = slides[slides.indexOf(currentSlide)+1];
-    document.getElementById(currentSlide).style.display="block";
-  } else {
-    alert("End of slides");
-  }
-}
-//previousSlide function
-function previousSlide() {
-  if ((slides.indexOf(currentSlide)) > 0) {
-    document.getElementById(currentSlide).style.display="none";
-    currentSlide = slides[slides.indexOf(currentSlide)-1];
-    document.getElementById(currentSlide).style.display="block";
-  } else {
-    // If no previous slide available, Ask user if they want to return to nobleguy.me
-    if (confirm("Are you sure you want to abandon progress and return to www.nobleguy.me?")) {
-      window.location.href = "http://www.nobleguy.me";
-    }
+var attIDArray = ['L1','L2','L3','R1','R2','R3'];
+var inputIDArray = ['att1','att1','att2','att2','att3','att3'];
 
+function chooseSlide(evt, num) {
+  var slideNum = parseInt(num);
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
   }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  // if slide3 is clicked, update slider text
+  if (slideNum == 3) {
+    for (i = 0; i < attIDArray.length; i++) {
+      if (document.getElementById(inputIDArray[i]).value != ""){
+        document.getElementById(attIDArray[i]).innerHTML = document.getElementById(inputIDArray[i]).value;
+      }
+    }
+  }
+  // Show the current tab, and add an "active" class to the link that opened the tab
+  document.getElementById("slide"+ slideNum).style.display = "block";
+  evt.currentTarget.className += " active";
 }
+// Get the element with id="slide1" and click on it
+document.getElementById("defaultSlide").click();
+//nextSlide function
 function updateGraph() {
 // clear graph
 // using number of keys in userNums as a counter, iterate through
 // slides creating tree graph of userNums values
 // e.g. for slide in slides {create block for usernums[slide]}
-
-// for now, update graph just creates a div of usernums
-var node = document.createElement("span");
-var textNode = document.createTextNode(currentSlide+' = '+userNums[currentSlide]);
-node.appendChild(textNode);
-document.rightSide.appendChild(node);
 }
 
 function storeNum() {
-  // Stores user input into dictionary userNums
-  userNums[currentSlide] = document.getElementById(currentSlide + "input").value;
-  nextSlide();
-  updateGraph(); //TODO create function to rebuild graph each time a number is added
-  //alert(userNums[currentSlide]);
+  // Stores user input
 }
